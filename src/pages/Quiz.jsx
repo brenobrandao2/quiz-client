848bfc5b0b87e50b2840c4a9588c1f84c9c5b649
brenobrandao2 <breno.brandao@prodap.com.br>
2021-10-06 19:11:38 -0300

    feature: Cria contato na active campaign

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -3,8 +3,8 @@ import { useParams } from 'react-router-dom'
 import '../css/Quiz.css'
 import { Quiz as QuizClass, getById } from '../repository/quiz.repository'
 
-import imagem from '../assets/card02.png'
 import { Pergunta } from '../repository/pergunta.repository';
+import { createContact } from '../repository/contact.repository';
 
 const Quiz = () => {
     const { key } = useParams()
@@ -15,6 +15,8 @@ const Quiz = () => {
     const [countPerguntas, setCountPerguntas] = useState(0)
     const [showCardFinal, setShowCardFinal] = useState(false)
     const [flow, setFlow] = useState('')
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
 
     const nextQuestion = (indexResposta) => {
         const newFlow = flow + `${countPerguntas}${indexResposta}`
@@ -29,9 +31,15 @@ const Quiz = () => {
         }
     }
 
-    const finishQuiz = () => {
+    const finishQuiz = async (e) => {
+        setLoading(true)
+        e.preventDefault();
         const redirecionamento = quiz.fluxos[flow].redirecionamento || 'https://lifeandmoney.com.br/'
+        console.log(redirecionamento)
+        await createContact(name, email, quiz._id)
         window.location.href = redirecionamento
+        setLoading(false)
+        return false
     }
 
     useEffect(() => {
@@ -63,9 +71,9 @@ const Quiz = () => {
                             <p className="Quiz-subtitle">{quiz.subtitulo}</p>
                         </div>
                         <div className="Quiz-quizArea">
-                            <div className="Quiz-imageArea">
-                                <img src={imagem} alt='quiz_img' className="Quiz-image"/>
-                            </div>
+                            {quiz.imagem && <div className="Quiz-imageArea">
+                                <img src={`data:${quiz.imagem.mimetype};base64,${quiz.imagem.buffer}`} alt='quiz_img' className="Quiz-image"/>
+                            </div>}
                             <div className="Quiz-card">
                                 <div className="Quiz-progressBar">
                                     <p className="Quiz-progressText">{countPerguntas+1} / {perguntas.length}</p>
@@ -90,25 +98,33 @@ const Quiz = () => {
                         </div>
                     </div>    
                 : 
-                    showCardFinal ?
+                    loading ?
+                        <div className="QuizList-LoaderArea">
+                            <div className="QuizList-loader" />
+                        </div>
+                        :
                         <div className="Quiz-cardFinal">
                             <div className="Quiz-imageAreaCF">
-                                <img src={imagem} alt='quiz_img' className="Quiz-imageCF"/>
+                                {quiz.cardFinal.imagem && <img src={`data:${quiz.cardFinal.imagem.mimetype};base64,${quiz.cardFinal.imagem.buffer}`} alt='quiz_img' className="Quiz-imageCF"/>}
                             </div>
                             <div>
                                 <p className="Quiz-tituloCF">{quiz.cardFinal.titulo}</p>
                                 <p className="Quiz-subtituloCF">{quiz.cardFinal.subtitulo}</p>
                             </div>
-                            <input type="text" placeholder="Insira seu nome" className="Quiz-input" />
-                            <input type="text" placeholder="Insira seu melhor e-mail" className="Quiz-input" />
-                            <input type="button" value={quiz.cardFinal.botao} className="Quiz-finalButton" onClick={() => finishQuiz()}/>
-                        </div>
-                    :
-                        <div className="QuizList-LoaderArea">
-                            <div className="QuizList-loader" />
+                            <form onSubmit={(e) => finishQuiz(e)}>
+                                <input autoComplete="on" name="name" type="text" placeholder="Insira seu nome" className="Quiz-input" required onChange={(e) => setName(e.target.value)}/>
+                                <input autoComplete="on" name="email" type="email" placeholder="Insira seu melhor e-mail" className="Quiz-input" required onChange={(e) => setEmail(e.target.value)}/>
+                                <input type="submit" value={quiz.cardFinal.botao} className="Quiz-finalButton" />
+                            </form>
                         </div>
                 }
-
+                {!loading && <div className="Quiz-footer">
+                    <p className="Quiz-direitos">LIFE + MONEY · 2020 © Todos os direitos reservados</p>
+                    <div className="Quiz-footerLinks">
+                        <a href="https://lifeandmoney.com.br/termos-e-condicoes/" className="Quiz-footerLink">Termos e Condições Gerais de Uso</a>
+                        <a href="https://lifeandmoney.com.br/politica-de-privacidade/" className="Quiz-footerLink">Política de Privacidade</a>
+                    </div>
+                </div>}
             </div>
     )
 }
---
+++ src/repository/contact.repository.js
@@ -0,0 +1,19 @@
+export const createContact = async (name, email, id_quiz) => {
+    const opt = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            name,
+            email,
+            id_quiz
+        })
+    }
+    
+    try {
+        await fetch('http://137.184.132.242:3001/contacts/create', opt).then(async response => console.log(await response.json()))
+    } catch (error) {
+        console.log(error)
+    }
+}
