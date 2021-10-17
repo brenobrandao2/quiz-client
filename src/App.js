e1c31ba10f988737079545774846ca4d24616bd5
brenobrandao2 <breno.brandao@prodap.com.br>
2021-10-17 11:09:23 -0300

    feature: Adiciona favicon

--- src/App.js
+++ src/App.js
@@ -14,6 +14,8 @@ function App() {
           <Helmet>
             <meta charSet="utf-8" />
             <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
+            <link rel="icon" type="image/png" href="https://lifeandmoney.com.br/wp-content/uploads/2021/04/Favicon-LM-300x300.png" />
+            <title>Life + Money Quiz</title>
           </Helmet>
           <Header />
           <Route path="/:key" exact component={Quiz} />
