6f126922d017ad052a2f1566f3553430db455f4a
brenobrandao2 <breno.brandao@prodap.com.br>
2021-10-18 17:15:29 -0300

    feature: Atualiza urls

--- src/utils/baseUrls.js
+++ src/utils/baseUrls.js
@@ -1,5 +1,5 @@
 const lam_server = 'https://lifeandmoney.com.br/quiz-client-service'
 const dev_server = 'https://brenobrandao.site/quiz-client-service'
 
-export const base_url_client_quiz = 'https://quiz.brenobrandao.site/'
-export const base_url_db = dev_server
+export const base_url_client_quiz = 'https://cartoes.lifeandmoney.com.br/'
+export const base_url_db = lam_server
