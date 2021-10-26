ae7e340e3acf6f85ee746cb6c05aa062e5f7b62d
brenobrandao2 <breno.brandao@prodap.com.br>
2021-10-26 18:24:44 -0300

    fix: Ajusta baseUrls

--- src/utils/baseUrls.js
+++ src/utils/baseUrls.js
@@ -1,4 +1,4 @@
-const lam_server = 'https://lifeandmoney.com.br/quiz-client-service'
+const lam_server = 'https://cartoes.lifeandmoney.com.br/quiz-client-service'
 const dev_server = 'https://brenobrandao.site/quiz-client-service'
 
 export const base_url_client_quiz = 'https://cartoes.lifeandmoney.com.br/'
