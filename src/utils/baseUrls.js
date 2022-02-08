a3587dffd96bf750814b96136697f3dbaec8764e
brenobrandao2 <breno.brandao@prodap.com.br>
2022-02-08 20:47:00 -0300

    fix: Atualiza urls para lifeandmoney

--- src/utils/baseUrls.js
+++ src/utils/baseUrls.js
@@ -1,6 +1,6 @@
-const lam_server = 'https://cartoes.pontosemilhas.com/quiz-client-service'
+const lam_server = 'https://cartoes.lifeandmoney.com.br/quiz-client-service'
 const dev_server = 'https://brenobrandao.site/quiz-client-service'
 const local_server = 'http://localhost:3002'
 
-export const base_url_client_quiz = 'https://cartoes.pontosemilhas.com/'
+export const base_url_client_quiz = 'https://cartoes.lifeandmoney.com.br/'
 export const base_url_db = lam_server
